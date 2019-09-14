7a6d094e2cc8c7ffa4c005362879e6adf9e202cb
Sean Faulkner <sean.faulkner@my.tccd.edu>
2019-09-13 19:17:52 -0500

    Add project files.

---
+++ Itse1430.Maze/Program.cs
@@ -0,0 +1,17 @@
+﻿// Sean Faulkner
+//
+//
+//
+
+using System;
+
+namespace Itse1430.Maze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
